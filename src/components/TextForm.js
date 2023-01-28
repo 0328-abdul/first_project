5a5148d31ef81f180c5197a3c479378868681bc7
0328-abdul <0328abdul@gmail.com>
2023-01-28 18:30:43 +0530

    Adding More Logic

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,16 +6,27 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText)
     }
+    const handleLowClick = () => {
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
     const handleOnChange = (e) => {
         setText(e.target.value)
     }
     return (
-        <div>
-            <h3>{props.heading}</h3>
-            <div className="form-group purple-border mb-2">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea4" rows="8"></textarea>
+        <>
+            <div className='container'>
+                <h3>{props.heading}</h3>
+                <div className="form-group purple-border mb-2">
+                    <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea4" rows="8"></textarea>
+                </div>
+                <button className='btn btn-sm btn-primary mx-1' onClick={handleUpClick}>Uppercase</button>
+                <button className='btn btn-sm btn-primary mx-1' onClick={handleLowClick}>Uppercase</button>
             </div>
-            <button className='btn btn-sm btn-primary' onClick={handleUpClick}>Uppercase</button>
-        </div>
+            <div className="container my-3">
+                <h4>Your Text Summary</h4>
+                <p>{text.split(" ").length} words and {text.length} characters..</p>
+            </div>
+        </>
     )
 }
